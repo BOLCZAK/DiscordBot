ff3ca31a3acf3e8d8c349e1a1bf92d06165b3782
Michal Olczak <micolo2000@gmail.com>
2020-09-21 04:39:12 +0200

    Added google search functionality and .gitignore file

--- cogs/web.py
+++ cogs/web.py
@@ -2,6 +2,8 @@ import discord, random
 from discord.ext import commands
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from googlesearch import search
+from time import sleep
 
 class Web(commands.Cog):
 
@@ -25,6 +27,21 @@ class Web(commands.Cog):
         tekst = tekst.replace('&nbsp;', ' ')
 
         await ctx.send(tekst)
+
+    @commands.command(aliases = ['wyszukaj', 'znajdź', 'znajdz'], help = 'Smolin szuka podanej frazy w google\nSkładnia: "[Prefix] google ilość zapytanie"\nilość - ilość wyników jakie chcemy zobaczyć')
+    async def google(self, ctx, amount, *, question):
+        # browser = Web.driver
+        # browser.get("https://google.pl")
+        # browser.find_element_by_xpath('/html/body/div[2]/div[2]/form/div[2]/div[1]/div[1]/div/div[2]/input').send_keys(question)
+        # browser.find_element_by_xpath('/html/body/div[2]/div[2]/form/div[2]/div[1]/div[3]/center/input[1]').click()
+        # output = browser.find_element_by_xpath('/html/body/div[7]/div[2]/div[10]/div[1]/div[2]/div/div[2]/div[2]/div/div/div[1]/div/div[1]/a/h3').get_attribute('innerHTML')
+
+        results = search(question, tld="pl", lang="pl", start=0, stop=int(amount), pause=2)
+
+        for index, query in enumerate(results):
+            await ctx.send(f'{index+1} wynik wyszukiwania google:\n{query}')
+            sleep(1)
+        
 
 
 
